e6710d25df75c30a1a0b1646c54b64836d7ce183
veranika-savanchuk <veranika.savanchuk@adevinta.com>
2020-11-16 17:38:05 +0300

    Adde import

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
 import axios from 'axios';
+import mime from "mime-types";
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -41,9 +42,13 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
+
+      const contentType = mime.lookup(file.name);
+
+      const result = await axios(response.data, {
         method: 'PUT',
-        body: file
+        headers: { 'Content-type':  contentType },
+        data: file
       })
       console.log('Result: ', result)
       setFile('');
